a6fd057c1024c8b04cef6a7b71fbc26a6890c224
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2026-03-27 22:53:20 +0530

    Added Profile Photo and code for more beautify.

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const profilePic = document.getElementById("profile-pic");
+
+    // Path to your local image
+    profilePic.src = "images/profile.jpeg";
+});
